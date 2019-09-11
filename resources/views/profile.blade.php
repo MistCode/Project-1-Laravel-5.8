af5490bb3a22d3e5d588bb09277a6cfd832332e6
Arturo <arturovielma41@gmail.com>
2019-09-11 16:04:26 -0430

    modificando como se ve el perfil pero aun nada con lo de listar los datos de mensaje/comunidad

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.app')
- 
+
+@section('title','Perfil')
+
 @section('content')
 
 <div class="container">
   <div class="row">
-    <div class="col-md-8 col-md-offset-2">
+    <div class="col-md-8 col-md-offset-2" style="margin-top: 70px; margin-left: 70px;">
 
       @include('common.errors')
+      <div class="row no-gutters">
+    <div class="col-md-3">
         <img width="100px" height="100px" style="margin-top: 10px;" src="{{ asset('images/ae/'.$user->avatar) }}">
-        <h2>{{ $user->name }}</h2><br>
-        <h4>Cambiar Avatar</h4>
+        <h2 style="margin-top: 10px;">{{ $user->name }}</h2><br>
+    </div>
+      <div class="col-md-7">  
+        <h2 style="margin-top: 10px; margin-bottom: 20px;">Cambiar Avatar</h2>
           {{ Form::open(['route' => ['user.profile.update'], 'files' => true, 'method' => 'PATCH']) }}
             <div class="form-group">
               {{ Form::file('avatar') }}
@@ -17,7 +23,9 @@
             {{ Form::submit('Actualizar', ['class' => 'btn btn-primary'] , ['name' => 'submit']) }}
             <a href="{{ url('/grupos') }}" class="btn btn-success">Regresar</a>
           {{ Form::close() }}
+      </div>
     </div>
+  </div>
   </div>
 </div>
 
